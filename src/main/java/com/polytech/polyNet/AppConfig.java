12b0d7e5592af244bcad730abeb1c36f0c0da756
zeroual <abid1991@hotmail.fr>
2019-05-14 11:59:15 +0200

    step4

--- src/main/java/com/polytech/polyNet/AppConfig.java
+++ src/main/java/com/polytech/polyNet/AppConfig.java
@@ -1,17 +1,31 @@
 package com.polytech.polyNet;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class AppConfig {
 
     @Bean
     StoryRepository storyRepository() {
-        return new InMemoryStoryRepository();
+        return new JdbcStoryRepository(datasource());
     }
+
     @Bean
-    PublicationService publicationService(){
+    DataSource datasource() {
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setURL("jdbc:mysql://localhost:3306/polyNet");
+        dataSource.setUser("root");
+        dataSource.setPassword("root");
+        dataSource.setDatabaseName("polyNet");
+        return dataSource;
+    }
+
+    @Bean
+    PublicationService publicationService() {
         return new PublicationServiceImpl(storyRepository());
     }
 }
---
+++ src/main/java/com/polytech/polyNet/JdbcStoryRepository.java
@@ -0,0 +1,59 @@
+package com.polytech.polyNet;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JdbcStoryRepository implements StoryRepository {
+
+
+    private DataSource dataSource;
+
+    public JdbcStoryRepository(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public List<Story> findAll() {
+
+        List<Story> results = new ArrayList();
+        try {
+            //GET CONNECTION
+            Connection connection = dataSource.getConnection();
+            String sql = "SELECT * FROM STORY";
+            //EXECUTE QUERY
+            ResultSet resultSet = connection.createStatement().executeQuery(sql);
+
+            // FOR EACH ROW IN DATA BASE
+            while (resultSet.next()) {
+                // I GET THE CONTENT COLUMN
+                String content = resultSet.getString("CONTENT");
+                Story story = new Story(content);
+                // I ADD THE STORY TO THE RESULT
+                results.add(story);
+
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return results;
+    }
+
+    @Override
+    public void save(Story story) {
+        try {
+            //GET CONNECTION
+            Connection connection = dataSource.getConnection();
+            String sql = "INSERT INTO STORY(CONTENT) VALUES('" + story.getContent() + "')";
+            //EXECUTE QUERY
+            connection.createStatement().execute(sql);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
